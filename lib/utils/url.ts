81da21744a04e0cd42716cb70b4cdee5f191b6df
Anuj Goenka <anujgoenka06@gmail.com>
2025-08-03 22:29:06 -0700

    Refactor URL utility to simplify BASE_URL handling and ensure trailing slash consistency

--- lib/utils/url.ts
+++ lib/utils/url.ts
@@ -1,16 +1,8 @@
 export const getURL = () => {
-  let url =
-    process?.env?.BASE_URL ?? // Set this to your site URL in production env.
-    process?.env?.VERCEL_URL ?? // Automatically set by Vercel
-    'http://localhost:3000/'
-  
-  // Make sure to include `https://` when not localhost.
-  url = url.startsWith('http') ? url : `https://${url}`
+  const url = process.env.BASE_URL!
   
   // Make sure to include a trailing `/`.
-  url = url.endsWith('/') ? url : `${url}/`
-  
-  return url
+  return url.endsWith('/') ? url : `${url}/`
 }
 
 export const getAuthCallbackURL = (redirect?: string, priceId?: string) => {
